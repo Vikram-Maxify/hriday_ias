0299a3443390747fad6ce53aa511967f57c09304
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-05-11 22:26:02 +0530

    done

--- models/Scholarship.js
+++ models/Scholarship.js
@@ -5,39 +5,7 @@ const scholarshipSchema = new mongoose.Schema(
     fullName: { type: String,required: true },
     whatsappNumber: { type: String,  },
     email: { type: String,  },
-    currentLocation: { type: String,  },
 
-    educationStatus: {
-      type: String,
-      enum: [
-        "Undergraduate",
-        "Final Year Student",
-        "Graduate",
-        "Working Professional",
-      ],
-    },
-
-    upscAttemptTarget: {
-      type: String,
-      enum: ["2027", "2028", "2029+"],
-    },
-
-    appearedBefore: {
-      type: String,
-      enum: ["Yes", "No"],
-    },
-
-    medium: {
-      type: String,
-      enum: ["English", "Hindi", "Hinglish"],
-    },
-
-    studyMode: {
-      type: String,
-      enum: ["Online", "Offline"],
-    },
-
-    reason: { type: String,  },
   },
   { timestamps: true }
 );
