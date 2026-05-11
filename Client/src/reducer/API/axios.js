0d3a9bf9128a442b191adcc4514510a0cd3c6602
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-05-11 22:52:32 +0530

    form changed

--- Client/src/reducer/API/axios.js
+++ Client/src/reducer/API/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    // baseURL: "http://localhost:5000/api", // change in production
-    baseURL:"/api",
+    baseURL: "http://localhost:5000/api", // change in production
+    // baseURL:"/api",
     headers: {
         "Content-Type": "application/json",
     },
